25a72d1271fdb352ceff3230847f7141922a62a2
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-23 00:05:49 +0530

    Update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import client from "../client";
+import { Link } from "react-router-dom"
 
 const Blog = () => {
+
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    client.fetch(
+      `[_type=="post"]{
+        title,
+        slug,
+        body,
+        mainImage {
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`
+    ).then((data) => setPosts(data)).catch(console.error)
+  }, []);
+
+
   return (
-    <div>Blog</div>
+    <>
+      <section>
+        <h1>Blog Page</h1>
+        <h2>Post is {posts.length}</h2>
+
+        <div>
+          {posts.map((post) => (
+            console.log(post)
+         
+          ))}
+        </div>
+      </section>
+    </>
   )
 }
 
