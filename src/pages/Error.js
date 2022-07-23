2b2899dd58c25a3ca096818c93cc5ff1ab8d3019
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-23 11:01:23 +0530

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
-const Error = () => {
+export default function Error() {
   return (
-    <div>Error</div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="uppercase font-bold text-4xl tracking-wide mb-5 md:text-6xl ">
+        Error 404 | Page Not Found
+      </h1>
+      <Link
+        to="/"
+        className="py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold"
+      >
+        Back to homepage
+      </Link>
+    </div>
   )
 }
-
-export default Error
