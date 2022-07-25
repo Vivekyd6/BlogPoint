46f5ffb06b8f720cb45a50d9320f1265d5e8b28c
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-25 09:46:42 +0530

    Update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -14,6 +14,7 @@ export default function Blog() {
         title,
         time,
         slug,
+        description,
         body,
         mainImage {
           asset->{
@@ -74,6 +75,9 @@ export default function Blog() {
                 {/* <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {post.body.slice(0,50)}
               </p> */}
+              <p className="m-2">
+                {post.description}
+              </p>
 
                 <Link className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" to={`/blog/${post.slug.current}`}>
                   Read More <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
